1171c4fa82b0be0097924cb266c88cb574ee67ce
Guest User <Guest@epicodus-12.local>
2018-03-26 16:51:09 -0700

    to do list adds list.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Task } from './models/task.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  currentFocus: string = 'To NOT Do List: an exercise in Angular 5';
+  currentTime = new Date();
+  month: number = this.currentTime.getMonth() + 1;
+  day: number = this.currentTime.getDate();
+  year: number = this.currentTime.getFullYear();
+  newTask: Task[] = [
+    new Task("go to the lodge"),
+    new Task("eat a pane-pane sandwich"),
+    new Task("take some nootropics")
+  ];
+
+  addTask(){
+  alert(${`"https://vignette.wikia.nocookie.net/theloudhouse/images/a/a1/How-about-no-gif-dr-evil-4.gif/revision/latest?cb=20180202204327"}`);
+  }
 }
